0bb5864d528885b44ebaaf6ffee3ea16f7e31085
hel1yeah <hel1_yeah@ukr.net>
2021-07-01 22:58:32 +0300

    $ref

--- 3_lesson_data/app.js
+++ 3_lesson_data/app.js
@@ -1,7 +1,24 @@
 Vue.createApp({
   data: () => ({
-    title: 'v-text',
-    title2: 'title-2',
-    myHTML: '<h1> vue 3 app</h1>  '
+    title: "v-text",
+    title2: "title-2",
+    myHTML: "<h1> vue 3 app</h1>  ",
+    person: {
+      firstName: "Yura",
+      lastName: "Larsen",
+      age: 22,
+    },
+    items: [1, 2, 3],
   }),
+  methods: {
+    addItem() {
+      this.items.unshift(this.$refs.myInput.value);
+      this.$refs.myInput.value = "";
+    },
+  },
+  computed: {
+    eventItems() {
+      return this.items.filter((i) => i % 2 === 0);
+    },
+  },
 }).mount("#app");
