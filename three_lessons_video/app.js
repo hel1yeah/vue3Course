ca798415237b17096a176ee0d39ca20afc9c38e0
hel1yeah <hel1_yeah@ukr.net>
2021-07-02 21:21:03 +0300

    proxy

--- three_lessons_video/app.js
+++ three_lessons_video/app.js
@@ -1,21 +1,35 @@
+const h = Vue.h;
+
 const app = Vue.createApp({
   data: () => ({
     title: "Property title with data",
   }),
-  template: `
-    <div class='card center' style="color: red">
-      <h1>{{title}}</h1>
-      <button class='btn primary' 
-      @click=" foo "> 
-      Button change
-      </button>
-    </div>  
-  `,
+  // template: `
+  //   <div class='card center' style="color: red">
+  //     <h1>{{title}}</h1>
+  //     <button class='btn primary'
+  //     @click=" this.title = 'new title!!!!'">
+  //     Button change
+  //     </button>
+  //   </div>
+  // `,
   methods: {
-    foo(){
-      this.title = 'new title!!!!'
-      console.log('bar');
-    }
+    changeTitle() {
+      this.title = "изменили";
+      console.log(this);
+    },
+  },
+  render() {
+    return h(
+      "div",
+      {
+        class: "card center",
+      },
+      [
+        h("h1", {}, this.title),
+        h("button", { class: "btn", onclick: this.changeTitle }, "изменить "),
+      ]
+    );
   },
 });
 
