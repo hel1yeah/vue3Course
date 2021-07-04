24676dd13174b8dbbac13763b1e33f2c29842802
hel1yeah <hel1_yeah@ukr.net>
2021-07-04 18:16:00 +0300

    lesson slot

--- first-lessons-vue-cli/src/App.vue
+++ first-lessons-vue-cli/src/App.vue
@@ -1,84 +1,18 @@
 <template>
   <div class="container pt-1">
     <div class="card">
-      <h2>Актуальные новости {{ now }}</h2>
-      <h2>Открыто {{ openRate }} ||| Прочитано {{ readRate }}</h2>
-    </div>
+      <h2>
+        <code> Изучаем слот: {{ ' <slot /> ' }} </code>
+      </h2>
 
-    <app-news
-      v-for="item in news"
-      :key="item.id"
-      :title="item.title"
-      :id="item.id"
-      :is-open="item.isOpen"
-      :read-rate="readRate"
-      :was-read="item.wasRead"
-      :news="news"
-      @open-news="openNews"
-      @read-new="readNew"
-      @unmark="unmark"
-    >
-    </app-news>
+    </div>
   </div>
 </template>
 
 <script>
-import AppNews from "@/components/AppNews.vue";
 
 export default {
-  name: "App",
-  components: {
-    AppNews,
-  },
-  data() {
-    return {
-      openRate: 0,
-      readRate: 0,
-      now: new Date().toLocaleDateString(),
-      news: [
-        {
-          title: "Первая новость",
-          id: 1,
-          isOpen: false,
-          wasRead: false,
-        },
-
-        {
-          title: "вторая новость",
-          id: 2,
-          isOpen: true,
-          wasRead: false,
-        },
-        {
-          title: "третяя новость",
-          id: 3,
-          isOpen: false,
-          wasRead: false,
-        },
-      ],
-    };
-  },
-  provide() {
-    return {
-      title: "app provide",
-      news: this.news,
-    };
-  },
-  methods: {
-    openNews() {
-      this.openRate++;
-    },
-    readNew(id) {
-      const index = this.news.findIndex((news) => news.id === id);
-      this.news[index].wasRead = true;
-      this.readRate++;
-    },
-    unmark(id) {
-      const index = this.news.findIndex((news) => news.id === id);
-      this.news[index].wasRead = false;
-      this.readRate--;
-    },
-  },
+  
 };
 </script>
 
---
+++ first-lessons-vue-cli/src/components/AppList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card">
+    <ul>
+      <li v-for="(item, i) in 5" :key="item">
+        <slot :iter="item" :inx='i'></slot>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default { name: "AppList" };
+</script>
+
+<style lang="scss" scoped></style>
