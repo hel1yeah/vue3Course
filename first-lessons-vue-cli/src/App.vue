27f7dcdd318d3c922fa280ff09c74ca84d38e489
hel1yeah <hel1_yeah@ukr.net>
2021-07-05 21:53:03 +0300

    keep-alive, slot, component :is

--- first-lessons-vue-cli/src/App.vue
+++ first-lessons-vue-cli/src/App.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="container pt-1">
     <div class="card">
-      <h2>
-        <code> Изучаем слот: {{ ' <slot /> ' }} </code>
-      </h2>
-
+      <h2>Динамические и асинхронные компоненты</h2>
+      <app-button :classBtn="classOne" @action="active = 'one'">
+        One
+      </app-button>
+      <app-button :classBtn="classTwo" @action="active = 'two'">
+        Two
+      </app-button>
     </div>
+    <keep-alive>
+      <component :is="componentName" />
+    </keep-alive>
   </div>
 </template>
 
 <script>
-
+import AppButton from "./components/AppButton.vue";
+import AppTextOne from "./components/AppTextOne.vue";
+import AppTextTwo from "./components/AppTextTwo.vue";
 export default {
-  
+  components: {
+    AppButton,
+    AppTextOne,
+    AppTextTwo,
+  },
+  data() {
+    return {
+      active: "one",
+    };
+  },
+  computed: {
+    componentName() {
+      return "app-text-" + this.active;
+    },
+    classOne() {
+      return this.active === "one" ? "primary" : "";
+    },
+    classTwo() {
+      return this.active === "two" ? "primary" : "";
+    },
+  },
 };
 </script>
 
