64d8dac8b9e30992bfbaf10a2fcf1dd0f981b0f3
hel1yeah <hel1_yeah@ukr.net>
2021-07-09 23:05:19 +0300

    use firebase

---
+++ 008lesson/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <form class="card" @submit.prevent="createPerson" >
+      <h2>Работа с базой данных</h2>
+      <div class="form-control">
+        <lable for="name">Введите имя</lable>
+        <input class="text" id="name" v-model.trim="name" />
+      </div>
+      <button class="btn primaty" :disabled="name.length === 0">
+        Создать человека
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      name: "",
+    };
+  },
+  methods: {
+    createPerson(){
+      // https://vuehttplesson-default-rtdb.europe-west1.firebasedatabase.app/
+
+    }
+  },
+};
+</script>
+
+<style lang="scss"></style>
---
+++ 008lesson/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from "vue";
+
+import App from "./App.vue";
+
+import './assets/theme.css'
+
+const app = createApp(App);
+
+app.mount("#app");
