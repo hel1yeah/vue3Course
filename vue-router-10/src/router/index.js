108a84d4c83b1d42a506ae0f58f990a9ab16f086
Yura Larsen <50619201+hel1yeah@users.noreply.github.com>
2021-11-08 21:56:59 +0200

    test

---
+++ vue-router-10/src/router/index.js
@@ -0,0 +1,47 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import Login from './../views/Login.vue';
+import Forget from './../views/Forget.vue';
+// import Mail from './../views/Mail.vue';
+import Dashboard from './../views/Dashboard.vue';
+
+import AppEmailBody from './../components/AppEmailBody.vue';
+import TheNotFaund from './../components/TheNotFaund.vue';
+
+const routes = [
+  { path: '/login', name: 'login', component: Login, alias: '/' },
+  {
+    path: '/forget',
+    name: 'forget',
+    component: Forget,
+    meta: { cantEnter: true },
+  },
+  {
+    path: '/mail',
+    name: 'mail',
+    children: [{ path: ':mailId?', component: AppEmailBody, props: true }],
+    component: Mail,
+  },
+  { path: '/dashboard', name: 'dashboard', component: Dashboard },
+  {
+    path: '/:found(.*)',
+    component: TheNotFaund,
+  },
+];
+
+let router = createRouter({
+  history: createWebHistory(),
+  routes,
+  linkActiveClass: 'active',
+  linkExactActiveClass: 'active',
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.cantEnter) {
+    next(false);
+  } else {
+    next();
+  }
+});
+
+export default router;
