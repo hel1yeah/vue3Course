0325d09286aa7688c8c4a4d32d3bb33708568eb0
Yura Larsen <hel1_yeah@ukr.net>
2021-11-16 19:02:37 +0200

    setup, ref

---
+++ compositionApiVue3/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <h1>Vue Composition Api</h1>
+      <small>data, methods, computed, watch</small>
+      <hr />
+      <p>
+        Название: <strong>{{ name }}</strong>
+      </p>
+      <p>
+        Версия: <strong>{{ version }}</strong>
+      </p>
+
+      <button class="btn" @click="changeInfo">Изменить</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const name = ref('VueJS!');
+    const version = ref(3);
+
+    return {
+      name,
+      version,
+    };
+  },
+
+  // data() {
+  //   return {
+  //     name: 'VueJS',
+  //     version: 3,
+  //   };
+  // },
+  // methods: {
+  //   changeInfo() {
+  //     this.name = 'Vue JS!';
+  //     this.version = 4;
+  //   },
+  // },
+};
+</script>
