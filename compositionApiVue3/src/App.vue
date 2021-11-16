352a2a8e6414e23b3a1979ff6d6125da305ee789
Yura Larsen <hel1_yeah@ukr.net>
2021-11-16 20:37:26 +0200

    доступ к DOM elements

--- compositionApiVue3/src/App.vue
+++ compositionApiVue3/src/App.vue
@@ -16,6 +16,10 @@
       </p>
 
       <button class="btn" @click="changeInfo">Изменить</button>
+
+      <div class="form-control">
+        <input class="input" type="text" ref="textInput" />
+      </div>
     </div>
   </div>
 </template>
@@ -27,15 +31,13 @@ export default {
   setup() {
     const name = ref('VueJS!');
     const version = ref(3);
-
-    // const framework = reactive({
-    //   name: 'VueJS!',
-    //   version: 3,
-    // });
+    const textInput = ref(null);
 
     function changeInfo() {
       name.value = 'Vue.js!!!';
       version.value = 42;
+
+      console.log(textInput.value.value);
     }
 
     const dobleVersion = computed(() => version.value * version.value);
@@ -51,6 +53,7 @@ export default {
       changeInfo,
       // framework,
       dobleVersion,
+      textInput,
     };
   },
 };
