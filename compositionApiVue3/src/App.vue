6dc900f9923b332e29f5dbf7389149d79f5087f5
Yura Larsen <hel1_yeah@ukr.net>
2021-11-18 22:32:40 +0200

    props, emits

--- compositionApiVue3/src/App.vue
+++ compositionApiVue3/src/App.vue
@@ -4,16 +4,6 @@
       <h1>Vue Composition Api</h1>
       <small>data, methods, computed, watch</small>
       <hr />
-      <p>
-        Название: <strong>{{ name }}</strong>
-      </p>
-      <p>
-        Версия: <strong>{{ version }}</strong>
-      </p>
-
-      <p>
-        Версия в квадрате: <strong>{{ dobleVersion }}</strong>
-      </p>
 
       <button class="btn" @click="changeInfo">Изменить</button>
 
@@ -21,13 +11,23 @@
         <input class="input" type="text" v-model="textInput" />
       </div>
     </div>
+    <framework-info
+      :name="name"
+      :version="version"
+      @changeBack="changeBack"
+    ></framework-info>
   </div>
 </template>
 
 <script>
 import { ref, reactive, toRefs, isRef, computed, watch } from 'vue';
 
+import FrameworkInfo from './FrameworkInfo.vue';
+
 export default {
+  components: {
+    FrameworkInfo,
+  },
   setup() {
     const name = ref('VueJS!');
     const version = ref(3);
@@ -36,24 +36,27 @@ export default {
     function changeInfo() {
       name.value = 'Vue.js!!!';
       version.value = 42;
-
-      console.log(textInput.value.value);
     }
 
-    const dobleVersion = computed(() => version.value * version.value);
+    function changeBack() {
+      version.value = 3;
+    }
 
-    watch([dobleVersion, textInput], (newValue, oldValue) => {
-      console.log(newValue[0]);
-      console.log(oldValue[1]);
-      console.log(newValue[1]);
-    });
+    // const dobleVersion = computed(() => version.value * version.value);
+
+    // watch([dobleVersion, textInput], (newValue, oldValue) => {
+    //   console.log(newValue[0]);
+    //   console.log(oldValue[1]);
+    //   console.log(newValue[1]);
+    // });
 
     return {
       name,
       version,
       changeInfo,
+      changeBack,
       // framework,
-      dobleVersion,
+      // dobleVersion,
       textInput,
     };
   },
---
+++ compositionApiVue3/src/FrameworkInfo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card">
+    <p>
+      Название: <strong>{{ name }}</strong>
+    </p>
+    <p>
+      Версия: <strong>{{ version }}</strong>
+    </p>
+    <p>
+      Версия в квадрате: <strong>{{ dobleVersion }}</strong>
+    </p>
+    <button class="btn danger" @click="changeBack">Change Back</button>
+  </div>
+</template>
+
+<script>
+import { computed, version } from 'vue';
+
+export default {
+  props: ['name', 'version'],
+  emits: ['changeBack'],
+  setup(props, context) {
+    const dobleVersion = computed(() => props.version * props.version);
+
+    function changeBack() {
+      context.emit('changeBack', 3);
+    }
+
+    return {
+      dobleVersion,
+      changeBack,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
