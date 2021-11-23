fdf0369f34ccedf314ea291f451c5778522820a7
Yura Larsen <hel1_yeah@ukr.net>
2021-11-23 23:41:53 +0200

    vuex created

--- compositionApiVue3/src/main.js
+++ compositionApiVue3/src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue';
 import './theme.css';
 
 import { router } from './router.js';
+import store from './store/store';
 
 createApp(App)
   .use(router)
+  .use(store)
   .mount('#app');
