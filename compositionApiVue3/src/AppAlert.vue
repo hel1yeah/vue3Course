1d73e7b3389b5d8e319f44e0e2bab7e65d17e0dd
Yura Larsen <hel1_yeah@ukr.net>
2021-11-23 22:24:18 +0200

    emits

---
+++ compositionApiVue3/src/AppAlert.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="alert" :class="type" @click="closeAlert">
+    <p class="alert-title">{{ alertTitle }}</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga labore
+      adipisci hic, nulla temporibus, distinctio ex nisi pariatur quisquam modi
+      quos consequuntur, sunt dignissimos reiciendis rem esse asperiores autem
+      ducimus. Dignissimos dolorem veniam itaque enim adipisci, deserunt nobis
+      earum possimus atque accusantium voluptatibus tempora qui illo doloremque
+      voluptates provident rem distinctio quisquam? Aperiam saepe minus commodi
+      laboriosam? Eum, quam accusantium.
+    </p>
+  </div>
+</template>
+
+<script>
+import { defineEmit } from 'vue';
+
+export default {
+  emits: ['closeAlert'],
+  props: ['alertTitle', 'type'],
+  setup() {
+    const emit = defineEmit(['closeAlert']);
+
+    const handleClose = () => {
+      emit('close');
+    };
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- compositionApiVue3/src/views/ReusablePage.vue
+++ compositionApiVue3/src/views/ReusablePage.vue
@@ -1,28 +1,44 @@
 <template>
+  <app-alert
+    v-if="alert"
+    alertTitle="work"
+    type="primary"
+    @closeAlert="closeAlert"
+  ></app-alert>
   <div class="card">
     <h1>ReusablePage</h1>
-    <button class="btn primary">Показ сообщений</button>
+    <button class="btn primary" @click="toggleAlert">
+      {{ alert ? 'Закрыть' : 'Показать' }} сообщений
+    </button>
     <button class="btn" @click="navigateTo">Перейти на главную</button>
   </div>
 </template>
 
 <script>
 import { useRouter, useRoute } from 'vue-router';
+import AppAlert from '../AppAlert.vue';
+import { ref } from 'vue';
 
 export default {
+  components: { AppAlert },
   setup() {
+    const alert = ref(false);
+
     const router = useRouter();
     const route = useRoute();
 
     console.log(route.path);
 
-    const navigateTo = () => {
-      console.log(route);
-      router.push('/');
-    };
+    const navigateTo = () => router.push('/');
 
+    const toggleAlert = () => (alert.value = !alert.value);
+
+    const closeAlert = () => (alert.value = false);
     return {
+      alert,
       navigateTo,
+      toggleAlert,
+      closeAlert,
     };
   },
 };
